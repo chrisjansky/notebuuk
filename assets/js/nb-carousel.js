8c35e2df45f80ef880c9bfef8b3cb6ac292616fd
chrisjansky <c.jansky@de.edenspiekermann.com>
2014-12-20 20:11:57 +0100

    JS SCSS Changed carousel to loop mode

--- assets/js/nb-carousel.js
+++ assets/js/nb-carousel.js
@@ -1,7 +1,6 @@
 var
   carouselStartClass = "carousel--start",
-  carouselEndClass = "carousel--end",
-  carouselArrowDisabledClass = "arrow--is-disabled"
+  carouselEndClass = "carousel--end"
 
 $("[data-carousel]").each(function(index, instance) {
   var carouselSwiper = new Swiper(instance, {
@@ -9,18 +8,8 @@ $("[data-carousel]").each(function(index, instance) {
     calculateHeight: true,
     keyboardControl: true,
     centeredSlides: true,
-
-    onFirstInit: function(swiper) {
-      carouselPosition(swiper);
-    },
-
-    onSlideChangeStart: function(swiper) {
-      carouselPosition(swiper);
-    },
-
-    onSlideReset: function(swiper) {
-      carouselPosition(swiper);
-    }
+    loop: true,
+    loopedSlides: 2
   });
 
   $(instance).find("[data-swiper--prev]").on("click", function(event) {
@@ -33,21 +22,3 @@ $("[data-carousel]").each(function(index, instance) {
   });
 
 });
-
-function carouselPosition(instance) {
-  var
-    firstIsActive = instance.activeIndex === 0,
-    lastIsActive = instance.activeIndex === instance.slides.length - 1
-
-  $(instance.container)
-    .toggleClass(carouselStartClass, firstIsActive)
-    .toggleClass(carouselEndClass, lastIsActive);
-
-  $(instance.container)
-    .find("[data-swiper--prev]")
-    .toggleClass(carouselArrowDisabledClass, firstIsActive);
-
-  $(instance.container)
-    .find("[data-swiper--next]")
-    .toggleClass(carouselArrowDisabledClass, lastIsActive);
-}

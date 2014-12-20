1b6b00ffaf84723f3901e7c225df2bb67e77fb11
chrisjansky <c.jansky@de.edenspiekermann.com>
2014-12-20 22:47:37 +0100

    JS Removed hash detection from nb-scrollto

--- assets/js/nb-scrollto.js
+++ assets/js/nb-scrollto.js
@@ -1,20 +1,7 @@
 // Scroll to anchor.
 $("[data-scrollto]").click(function() {
-  window.location.hash = "section-" + this.hash.slice(1)
-
   $("html, body").animate({
     scrollTop: $($(this).attr("href")).offset().top
   }, durBasic, transFastOut);
   return false
 });
-
-// On load.
-if (location.hash) {
-  var smoothScrollTo = "#" + location.hash.slice(9)
-
-  setTimeout(function() {
-    $("html, body").animate({
-      scrollTop: $(smoothScrollTo).offset().top
-    }, durBasic, transFastOut);
-  }, durLong)
-}

c18f717264c27b819444ddf57923652b8e189e60
chrisjansky <chrisjansky@gmail.com>
2014-12-24 00:03:16 +0100

    JS Improved waypoints

--- assets/js/nb-waypoints.js
+++ assets/js/nb-waypoints.js
@@ -6,23 +6,46 @@ $(document).on("ready", function() {
   if (isDesktop()) {
     $body.addClass("body--is-desktop");
 
-    var
-      waypointDown = $("[data-waypoint]").waypoint({
-        handler: function(direction) {
-          $(this.element).toggleClass("shapes--are-active", direction === "down")
-        },
-        offset: "50%"
-      })
+    // Add passive class on init.
+    // Fucking hard to manipulate classes on SVG. Using: https://github.com/JeremiePat/SVG-DOM-helpers/
+    var jsShapes = document.getElementsByClassName("js-shape");
+    Array.prototype.forEach.call(jsShapes, function(shape) {
+      shape.addClass("shape--is-passive");
+    });
 
     var
-      waypointUp = $("[data-waypoint]").waypoint({
+      shapeWaypointDown = $(".js-shape").waypoint({
         handler: function(direction) {
-          $(this.element).toggleClass("shapes--are-active", direction === "up")
+          var thisElement = this.element;
+
+          if (direction === "down") {
+            thisElement.addClass("shape--is-active");
+          } else {
+            thisElement.removeClass("shape--is-active");
+          }
+
         },
+        // Top hit 75% of the screen
+        offset: "75%"
+      });
+
+    var
+      shapeWaypointUp = $(".js-shape").waypoint({
+        handler: function(direction) {
+          var thisElement = this.element;
+
+          if (direction === "up") {
+            thisElement.addClass("shape--is-active");
+          } else {
+            thisElement.removeClass("shape--is-active");
+          }
+
+        },
+        // Middle of element hit 25% of the screen
         offset: function() {
-          return (Waypoint.viewportHeight() * .5) - this.element.clientHeight
+          return (Waypoint.viewportHeight() * .25) - (this.element.clientHeight * .5)
         }
-      })
+      });
 
   } else {
     $body.addClass("body--not-desktop");

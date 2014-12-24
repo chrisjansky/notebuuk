28ac509d113b990d492494679d3f0dcf86041947
chrisjansky <chrisjansky@gmail.com>
2014-12-25 00:51:14 +0100

    JS Completely rewritten nb-parallax

--- assets/js/nb-parallax.js
+++ assets/js/nb-parallax.js
@@ -1,97 +1,72 @@
-document.onreadystatechange = function() {
+function chjParallax(config) {
 
-  if (document.readyState === "complete" && isDesktop()) {
+  var defaults = {
+    selector: "[data-parallax]",
+    scrollSpeed: .25,
+    triggerOffset: .05
+  };
 
-    var config = {
-      selector: "[data-parallax]",
-      scrollSpeed: .25,
-      triggerOffset: .05
-    }
+  config = $.extend({}, defaults, config);
 
-    // Detect CSS Transform
-    var cssTransform = (function() {
-      var
-        prefixes = "transform webkitTransform mozTransform oTransform msTransform".split(" "),
-        el = document.createElement("div"),
-        cssTransform,
-        i = 0
+  var
+    viewHeight,
 
-      while (cssTransform === undefined) { 
-        cssTransform = document.createElement("div").style[prefixes[i]] != undefined ? prefixes[i] : undefined
-        i++
-      }
-      return cssTransform
-     })();
+    elements = document.querySelectorAll(config.selector),
+    elAmount = elements.length,
+    elMatrix = [];
 
-    // Detect request animation frame
+  // Detect CSS Transform
+  var cssTransform = (function() {
     var
-      scroll = window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || window.msRequestAnimationFrame || window.oRequestAnimationFrame || function(callback){ window.setTimeout(callback, 1000/60) },
-      lastPosition = -1,
-      wHeight,
-      elements = document.querySelectorAll(config.selector),
-      elAmount = elements.length,
-      matrix = []
+      prefixes = "transform webkitTransform mozTransform oTransform msTransform".split(" "),
+      el = document.createElement("div"),
+      cssTransform,
+      i = 0
 
-    // Pre calculate sizes to get better perfs
-    getParams = function() {
-      wHeight = window.innerHeight
-      trigger = Math.round(wHeight * config.triggerOffset)
-      lastPosition = -1 // Force a recalculation
-
-      var i = 0
-      for (i = 0; i < elAmount; i++) {
-        matrix[i] = matrix[i] || { el: elements[i] }
-
-        // Reinit 
-        matrix[i].el.style.display = "block"
-
-        // Start at 0 if it's the first in array
-        matrix[i].start = matrix[i-1] ? $(matrix[i].el).parent().offset().top - trigger : 0
-        matrix[i].stop = matrix[i].start + matrix[i].el.parentNode.offsetHeight + trigger
-      }
+    while (cssTransform === undefined) { 
+      cssTransform = document.createElement("div").style[prefixes[i]] != undefined ? prefixes[i] : undefined
+      i++
     }
+    return cssTransform
+   })();
 
-    function setTop(m, t) {
-      if (cssTransform)
-        m.el.style[cssTransform] = "translate3d(0, "+ t +"px,0)"
-      else
-        m.el.style["top"] = t
+  // Pre calculate sizes to get better perfs
+  this.getParams = function() {
+    viewHeight = window.innerHeight
+
+    var trigger = Math.round(viewHeight * config.triggerOffset)
+
+    var i = 0
+    for (i = 0; i < elAmount; i++) {
+      elMatrix[i] = elMatrix[i] || { el: elements[i] }
+
+      // Start at 0 if it's the first in array
+      elMatrix[i].start = elMatrix[i-1] ? $(elMatrix[i].el).parent().offset().top - trigger : 0
+      elMatrix[i].stop = elMatrix[i].start + elMatrix[i].el.parentNode.offsetHeight + trigger
     }
-
-    function loop() {
-      // Avoid calculations if not needed
-      if (lastPosition == window.pageYOffset) {
-        scroll(loop)
-        return false
-      } else lastPosition = window.pageYOffset
-
-      var i = 0
-      for (i = 0; i < elAmount; i++) {
-
-        // Check if visible
-        if (lastPosition >= matrix[i].start - wHeight && lastPosition <= matrix[i].stop){
-          matrix[i].el.style.display = "block"
-
-          setTop(matrix[i], (matrix[i].start - lastPosition) * config.scrollSpeed)
-        } else {
-          matrix[i].el.style.display = "none"
-        }
-      }
-      scroll(loop)
-    }
-
-    // Init
-    getParams()
-    loop()
-
-    // Get parameters again on resize debounce
-    $(window).on("resize orientationchange", function() {
-      clearTimeout(this.id)
-      this.id = setTimeout(function() {
-        getParams()
-      }, durBasic)
-    });
-
   }
-  
+  // Execute on init
+  this.getParams();
+
+  function setTop(m, t) {
+    if (cssTransform) 
+      m.el.style[cssTransform] = "translate3d(0, "+ t +"px,0)"
+    else
+      m.el.style["top"] = t
+  }
+
+  this.iterate = function(position) {
+    var i = 0
+    for (i = 0; i < elAmount; i++) {
+      // Check if visible
+      if (position >= elMatrix[i].start - viewHeight && position <= elMatrix[i].stop) {
+        elMatrix[i].el.style.opacity = "1"
+
+        setTop(elMatrix[i], (elMatrix[i].start - position) * config.scrollSpeed)
+      } else {
+        elMatrix[i].el.style.opacity = "0"
+      }
+    }
+  }
+
 }

fa2e19a3c402b48def5e7e83ad9d7cc2e3b2c141
chrisjansky <chrisjansky@gmail.com>
2014-12-25 00:51:29 +0100

    JS Added nb-loop

---
+++ assets/js/nb-loop.js
@@ -0,0 +1,44 @@
+document.onreadystatechange = function() {
+
+  if (document.readyState === "complete" && isDesktop()) {
+
+    var rAF = window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || window.msRequestAnimationFrame || window.oRequestAnimationFrame || function(callback){ window.setTimeout(callback, 1000/60) }
+
+    var
+      scrollPos = -1,
+      nbParallax = new chjParallax(),
+      nbChangeBg = new chjChangeBg();
+
+    (function nbLoop() {
+      if (scrollPos == window.pageYOffset) {
+        // Exit and run again
+        rAF(nbLoop);
+        return false;
+      } else {
+        scrollPos = window.pageYOffset;
+      }
+
+      nbParallax.iterate(scrollPos);
+      nbChangeBg.check(scrollPos);
+
+      rAF(nbLoop);
+    })();
+
+    // Get parameters again on resize debounce
+    $(window).on("resize orientationchange", function() {
+
+      clearTimeout(this.id)
+      
+      this.id = setTimeout(function() {
+        // Force recalculation
+        scrollPos = -1
+
+        nbParallax.getParams();
+        nbChangeBg.getParams();
+      }, durBasic)
+
+    });
+
+  }
+  
+}
